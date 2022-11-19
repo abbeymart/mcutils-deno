d7fb92bf4964be13ac5885f77ae7d827f8387be7
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2022-11-19 13:35:40 -0500

    updated-module-description

--- mod.ts
+++ mod.ts
@@ -1,20 +1,20 @@
 // Copyright 2022 mConnect.biz. All rights reserved. MIT license.
 
 /**
- * A test module testing Deno modules and applications.
+ * Shared Utility Functions for deno, nodejs, js and ts applications.
  *
  * ### Example Usage:
  *
  * Test case composition - see the test folder
  *
  * ```ts
- * import { assertEquals, assertNotEquals, postTestResult } from "https://deno.land/x/mctest/mod.ts";
+ * import { assertEquals, assertNotEquals, postTestResult } from "https://deno.land/x/mcutils/mod.ts";
  *
  * ```
  *
  * To test the module run:
  * ```sh
- * deno run test/testCases.ts
+ * deno run test.ts
  * ```
  *
  * @module
