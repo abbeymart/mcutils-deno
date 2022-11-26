59162f256251aed1a91828dd10665f36a44b305e
Abi Akindele <2366595+abbeymart@users.noreply.github.com>
2022-11-26 14:56:23 -0500

    refactored-deps-types

--- deps.ts
+++ deps.ts
@@ -3,4 +3,3 @@
 // export third party dependencies
 export { getResMessage } from "https://deno.land/x/mcresponse@v0.2.1/mod.ts";
 export type { ResponseMessage } from "https://deno.land/x/mcresponse@v0.2.1/mod.ts";
-export * from "https://deno.land/x/mctest@v0.1.4/mod.ts";
